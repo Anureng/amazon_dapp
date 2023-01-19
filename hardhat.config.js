a77fc20a7473e30e54fad8e0255dc8b83907ba88
Anurag Nitin Sidhu <anuragsidhu72@gmail.com>
2023-01-19 20:07:06 +0530

    Basic_Done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require('dotenv').config({ path: '.env' });
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  defaultNetwork: 'goerli',
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: process.env.ALCHEMY_KEY,
+      accounts: [process.env.SECRET_KEY]
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  }
 };
+
+// 0x805CF6449Ea9497a640d4119898FDa03624563fd
+
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navbar({ account }) {
+    return (
+        <div>{account}</div>
+    )
+}
+
+export default Navbar
